3558da71092d2fcb9f7d93e8aaa14a043e4df580
Aibek171 <129676487+Aibek171@users.noreply.github.com>
2023-04-02 19:55:45 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,5 +5,5 @@ import numpy as np
 chat_id = 1121374935 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    time=74 # По условию задачи 
+    time = 74 # По условию задачи 
     return (2*x/time**2).mean() # Ваш ответ
